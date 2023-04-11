e39516ba4a26113985de34d0041a452c7a626b41
hay1g21 <hay1g21@soton.ac.uk>
2023-04-11 17:52:31 +0100

    Datastore talks to client in new thread

--- src/DataStore.java
+++ src/DataStore.java
@@ -38,19 +38,33 @@ public class DataStore {
             InetAddress address = InetAddress.getLocalHost();
             controllerSocket = new Socket(address, cport); //sends to cport -controller listens at this port
             dSSocket = new ServerSocket(port); //port to listen for messages this way, make it server socket??
-            new Thread(new DataStoreReceiver(dSSocket,controllerSocket,uploadFolder)).start();
+            System.out.println("Connected to " + controllerSocket.getLocalPort());
+            new Thread(new ControllerListener(port,controllerSocket,uploadFolder)).start();
+
+            while(true) {
+                try {
+                    System.out.println("Back to accepting client connections");
+                    Socket client = dSSocket.accept(); //accepts a datastore socket
+                    new Thread(new DataStoreReceiver(dSSocket,controllerSocket,uploadFolder)).start();
+                }catch(Exception e) { System.err.println("error: " + e); }
+            }
+
+
+            //loops for connections
+
             //System.out.println("Socket started on port:" + dSSocket.getLocalPort());
-            PrintWriter out = new PrintWriter(controllerSocket.getOutputStream(), true);
+            //PrintWriter out = new PrintWriter(controllerSocket.getOutputStream(), true);
             //listens to textual messages from the controller
-            BufferedReader in = new BufferedReader(new InputStreamReader(controllerSocket.getInputStream()));
+            //BufferedReader in = new BufferedReader(new InputStreamReader(controllerSocket.getInputStream()));
             //listens to data messages from controller
-            InputStream inData = controllerSocket.getInputStream();
+            //InputStream inData = controllerSocket.getInputStream();
             //filereading test
 
-            File testFile = new File(uploadFolder+"/file2.txt");
-            System.out.println(testFile.getAbsolutePath());
-            FileOutputStream fileOut = new FileOutputStream(testFile);
+            //File testFile = new File(uploadFolder+"/file2.txt");
+            //System.out.println(testFile.getAbsolutePath());
+            //FileOutputStream fileOut = new FileOutputStream(testFile);
 
+            /*
             out.println(Protocol.JOIN_TOKEN + " " + port);
 
             while(true){
@@ -62,7 +76,7 @@ public class DataStore {
                     System.out.println(line + " received");
                     break;
                 }
-                /*
+
                 out.println("SEND");
                 //try reading a file, needs a fileoutput stream
                 //gets a buffer
@@ -76,16 +90,19 @@ public class DataStore {
 
                 }
 
-                 */
+
                 System.out.println("Done waiting, looping back");
                 //inData.close();
                 fileOut.close();
             }
-            
+
+
+             */
+
         } catch(Exception e) { System.err.println("error: " + e);
         } finally {
-            if (controllerSocket != null)
-                try { controllerSocket.close(); } catch (IOException e) { System.err.println("error: " + e); }
+            if (dSSocket != null)
+                try { dSSocket.close(); } catch (IOException e) { System.err.println("error: " + e); }
             if (!uploadFolder.exists()){
                 uploadFolder.delete();
             }
@@ -198,12 +215,13 @@ public class DataStore {
 
     static class ControllerListener implements Runnable{
 
-        ServerSocket ds;
+
         Socket cport; //server socket
+        int port; //port of ds
         File toStore;
 
-        ControllerListener (ServerSocket ds, Socket cport, File toStore){
-            this.ds = ds;
+        ControllerListener (int port, Socket cport, File toStore){
+            this.port = port;
             //listPorts.add(134);
             this.cport = cport;
             this.toStore = toStore;
@@ -211,13 +229,35 @@ public class DataStore {
 
         public void run() {
             try{
-                receiveMessage();
+                System.out.println("Controller started");
+                BufferedReader in = new BufferedReader(new InputStreamReader(cport.getInputStream())); //listens from port
+                PrintWriter out = new PrintWriter(cport.getOutputStream(), true); //prints to datastore, replies
+
+
+                //sends join message
+                out.println(Protocol.JOIN_TOKEN + " " + port);
+                while(true){
+                    //Thread.sleep(1000);
+                    System.out.println("I'm still alive");
+                    //get a connection to listen for messages
+                    String line;
+                    while((line = in.readLine()) != null) {
+                        System.out.println(line + " received");
+                        //break;
+                    }
+                    //out.println("SEND");
+                    //try reading a file, needs a fileoutput stream
+                    //gets a buffer
+                    System.out.println("Waiting for file");
+                    System.out.println("Done waiting, looping back");
+
+                }
             } catch(Exception e) { System.err.println("error: " + e);
             } finally {
-                if (ds != null)
+                if (cport != null)
                     try {
                         System.out.println("Server Socket Closing");
-                        ds.close();
+                        cport.close();
 
                     } catch (IOException e) { System.err.println("error: " + e); }
             }
@@ -225,77 +265,8 @@ public class DataStore {
         }
 
         public void receiveMessage(){
-            while(true) {
-                try {
 
-                    System.out.println("Accepting Connections from port: " + ds.getLocalPort());
-                    Socket client = ds.accept();
-                    System.out.println("Connection accepted : " + client);
-                    //Textual messages
-                    BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream())); //listens from port
-                    PrintWriter out = new PrintWriter(client.getOutputStream(), true); //prints to datastore, replies
-                    //Data messages (For testing)
-                    InputStream inData = client.getInputStream(); //gets
-                    OutputStream outData = client.getOutputStream(); //sends
-                    String line;
 
-                    //controller socket
-                    PrintWriter outC = new PrintWriter(cport.getOutputStream(), true); //prints to datastore, replies
-
-                    //out.println("Acknowledged connection to client");
-                    while ((line = in.readLine()) != null) {
-                        System.out.println(line + " received, now choosing what to do");
-                        if (line.contains("STORE")) {
-                            //get filename and size
-                            String[] split = line.split(" ");
-                            System.out.println("Storing file: " + split[1] + " with size " + split[2]);
-                            File newFile = new File(toStore + "/" + split[1]);
-                            byte[] buf = new byte[Integer.parseInt(split[2])];
-                            int buflen;
-
-                            FileOutputStream fileOut = new FileOutputStream(newFile);
-                            //send ack and prepare to read
-                            out.println(Protocol.ACK_TOKEN);
-                            //now read in file
-                            while ((buflen = inData.read(buf)) != -1) {
-                                System.out.println("*Writing*");
-                                fileOut.write(buf, 0, buflen);
-                                break;
-                            }
-                            System.out.println("Finished reading file " + newFile.getName());
-                            //send acknowledgement to controller
-                            outC.println(Protocol.STORE_ACK_TOKEN + " " + newFile.getName());
-                            fileOut.close();
-                        }else if(line.contains("LOAD_DATA")){
-                            System.out.println("Wants to load a file");
-                            String[] split = line.split(" ");
-                            System.out.println("Storing file: " + split[1]);
-
-                            File fileToLoad = new File(toStore + "/" + split[1]);
-                            if(fileToLoad.exists()){
-                                FileInputStream inp = new FileInputStream(fileToLoad);
-                                //time to write the file
-                                byte[] buf = new byte[1000];
-                                int buflen;
-                                while ((buflen = inp.read(buf)) != -1) {
-                                    System.out.println("*");
-                                    outData.write(buf, 0, buflen);
-                                }
-                                System.out.println("Finished writing");
-                                inp.close();
-                            }else{
-                                System.out.println("File does not exist");
-                            }
-                        } else {
-                            System.out.println("Nothing special with this line");
-                        }
-                    }
-                    System.out.println("Closing client");
-                    client.close();
-                } catch (Exception e) {
-                    System.err.println("error: " + e);
-                }
-            }
         }
 
     }
